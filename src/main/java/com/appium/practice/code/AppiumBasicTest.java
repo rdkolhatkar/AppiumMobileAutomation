8ebbfdce9c9c6439e35bbfd2af1f14f72e4801b7
Ratnakar <ratnakarkolhatkar@gmail.com>
2025-08-09 13:54:25 +0530

    Appium Important Notes and Code comments

--- src/main/java/com/appium/practice/code/AppiumBasicTest.java
+++ src/main/java/com/appium/practice/code/AppiumBasicTest.java
@@ -21,7 +21,7 @@ public class AppiumBasicTest {
         // In appium to start our test execution we have to configure the IOSDriver class for iOS devices.
         // Start the Appium Server on your local device
         AppiumDriverLocalService serviceBuilder = new AppiumServiceBuilder()
-                .withAppiumJS(new File("C:/Users/Ratnakar/AppData/Roaming/npm/node_modules/appium/build/lib/main.js"))
+                .withAppiumJS(new File("C:/Users/Ratnakar/AppData/Roaming/npm/node_modules/appium/build/lib/main.js")) // Appium requires the absolute path to the "appium/build/lib/main.js" file
                 .withIPAddress("127.0.0.1")
                 .usingPort(4723)
                 .withTimeout(Duration.ofSeconds(120))
@@ -30,7 +30,7 @@ public class AppiumBasicTest {
         // Create an instance of UiAutomator2Options to set the desired capabilities for the Android driver
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("MyAndroidDevice");
-        options.setApp("D:/Core Java/projects/AppiumMobileAutomation/src/main/resources/applications/ApiDemos-debug.apk");
+        options.setApp("D:/Core Java/projects/AppiumMobileAutomation/src/main/resources/applications/ApiDemos-debug.apk"); // Appium requires the absolute path to the ".apk" files
         AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
 
 
